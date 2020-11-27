bb9a601709bb3e98f337ee77a9bbdb0037a43e60
Michael Lutz <lutz.michael-ten18@it.dhbw-ravensburg.de>
2020-11-27 01:51:29 +0100

    Refinded test values and check for success in is_Phenomena().

--- pymeeus-master/tests/test_check_JupiterMoons.py
+++ pymeeus-master/tests/test_check_JupiterMoons.py
@@ -26,12 +26,14 @@ TOL_5 = 1e-5
 SATELLITES = ['Io', 'Europe', 'Ganymede', 'Callisto']
 COORDS_X_Y = ['X', 'Y']
 COORDS_X_Y_Z = ['X', 'Y', 'Z']
+EVENTS = ['OCC', 'ECC', 'N/A']
 
 TOL_TEST_ALL = [TOL_3, TOL_4, TOL_5]
 TOL_TEST_SIMPLE = [TOL_3, ]
 
 EPOCH_TEST_ALL = [EPOCH_1992_12_16, EPOCH_1992_12_16_UTC, EPOCH_1992_12_16_JD]
 EPOCH_TEST_SIMPLE = [EPOCH_1992_12_16, ]
+
 
 def msg(idx_sat: int, idx_coord, tol_threshold: float, comp_value: float, exp_value: float):
     return f"{SATELLITES[idx_sat]}: {COORDS_X_Y[idx_coord]}: " \
@@ -106,3 +108,55 @@ def test_correct_rectangular_positions(epoch, tol_threshold):
             check.less(abs(delta - exp_values[i][j]),
                        tol_threshold,
                        msg(i, j, tol_threshold, delta, exp_values[i][j]))
+
+
+def test_is_phenomena():
+    """This method tests the method correct_rectangular_positions() that corrects the rectangular geocentric
+    position of Jupiter's satellites for a given epoch as described
+    in astronomical algorithm chapter 44 page: 313 -314. """
+
+    """Matrix:
+        col 0: Label to identify test case
+        col 1: Epoch to be tested
+        col 2/3: row/col-index of expected 'True'
+    All other entries are expected to be 'False'!"""
+    test_values = [["IO-OCC-START_2021-01-17_00-55-11", Epoch(2021, 1, 17.0383217592593), 0, 0],
+                   ["IO-ECC-START_2021-02-12_14-19-14", Epoch(2021, 2, 12.5966898148148), 0, 1],
+                   ["EU-OCC-START_2021-01-26_19-50-36", Epoch(2021, 1, 26.8268055555556), 1, 0],
+                   ["EU-ECC-START_2021-02-13_14-26-18", Epoch(2021, 2, 13.6015972222222), 1, 1],
+                   ["GA-OCC-START_2021-01-01_18-56-58", Epoch(2021, 1, 1.78956018518519), 2, 0],
+                   ["GA-ECC-START_2021-02-06_16-53-50", Epoch(2021, 2, 6.70405092592593), 2, 1],
+                   ["CA-OCC-START_2021-03-09_13-26-47", Epoch(2021, 3, 9.5602662037037), 3, 0],
+                   ["CA-ECC-START_2021-03-09_07-00-20", Epoch(2021, 3, 9.29189814814815), 3, 1],
+                   ["IO-OCC-END_2021-02-14_11-20-01", Epoch(2021, 2, 14.4722337962963), 0, 0],
+                   ["IO-ECC-END_2021-01-27_18-15-09", Epoch(2021, 1, 27.7605208333333), 0, 1],
+                   ["EU-OCC-END_2021-01-30_12-07-38", Epoch(2021, 1, 30.5053009259259), 1, 0],
+                   ["EU-ECC-END_2021-01-16_06-54-18", Epoch(2021, 1, 16.2877083333333), 1, 1],
+                   ["GA-OCC-END_2021-12-18_23-58-55", Epoch(2021, 12, 18.9992476851852), 2, 0],
+                   ["GA-ECC-END_2021-12-19_04-36-37", Epoch(2021, 12, 19.1920949074074), 2, 1],
+                   ["CA-OCC-END_2021-11-15_07-29-43", Epoch(2021, 11, 15.3123032407407), 3, 0],
+                   ["CA-ECC-END_2021-11-15_19-49-16", Epoch(2021, 11, 15.8258796296296), 3, 1],
+                   ]
+
+    for epoch_label, epoch, row_idx, col_idx in test_values:
+        result = JupiterMoons.is_phenomena(epoch)
+        exp_event = False
+        other_events: [[]] = []
+        for i in range(len(result)):
+            events = result[i]
+            for j in range(len(events) - 1):
+                if result[i][j]:
+                    if i == row_idx and j == col_idx:
+                        exp_event = True
+                    else:
+                        other_events.append([SATELLITES[i], EVENTS[j]])
+
+        # assert success, "Test failed!"
+        # check.is_true(success, f"Test failed for {epoch_label}")
+        check.is_true(exp_event and len(other_events) == 0, f"Test={epoch_label}: "
+                                                            f"'expected event'={exp_event}, "
+                                                            f"'other event(s)'={other_events}")
+
+
+if __name__ == "__main__":
+    test_is_phenomena()
