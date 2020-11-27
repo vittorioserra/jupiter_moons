991ae19449fbf5b23d935c5dc204008a27d6691f
Michael Lutz <lutz.michael-ten18@it.dhbw-ravensburg.de>
2020-11-27 18:33:15 +0100

    Updated testset for occ/ecc-events.

--- pymeeus-master/tests/test_check_JupiterMoons.py
+++ pymeeus-master/tests/test_check_JupiterMoons.py
@@ -120,25 +120,25 @@ def test_is_phenomena():
         col 1: Epoch to be tested
         col 2/3: row/col-index of expected 'True'
     All other entries are expected to be 'False'!"""
-    test_values = [["IO-OCC-START_2021-01-17_00-55-11", Epoch(2021, 1, 17.0383217592593), 0, 0],
+    test_set = [["IO-OCC-START_2021-02-14_08-47-50", Epoch(2021, 2, 14.3665509259259), 0, 0],
                    ["IO-ECC-START_2021-02-12_14-19-14", Epoch(2021, 2, 12.5966898148148), 0, 1],
                    ["EU-OCC-START_2021-01-26_19-50-36", Epoch(2021, 1, 26.8268055555556), 1, 0],
                    ["EU-ECC-START_2021-02-13_14-26-18", Epoch(2021, 2, 13.6015972222222), 1, 1],
-                   ["GA-OCC-START_2021-01-01_18-56-58", Epoch(2021, 1, 1.78956018518519), 2, 0],
+                   ["GA-OCC-START_2021-01-01_03-56-31", Epoch(2021, 1, 1.16424768518519), 2, 0],
                    ["GA-ECC-START_2021-02-06_16-53-50", Epoch(2021, 2, 6.70405092592593), 2, 1],
-                   ["CA-OCC-START_2021-03-09_13-26-47", Epoch(2021, 3, 9.5602662037037), 3, 0],
-                   ["CA-ECC-START_2021-03-09_07-00-20", Epoch(2021, 3, 9.29189814814815), 3, 1],
+                   ["CA-OCC-START_2021-01-17_00-55-11", Epoch(2021, 1, 17.0383217592593), 3, 0],
+                   ["CA-ECC-START_2021-02-03_18-54-51", Epoch(2021, 2, 3.78809027777778), 3, 1],
                    ["IO-OCC-END_2021-02-14_11-20-01", Epoch(2021, 2, 14.4722337962963), 0, 0],
-                   ["IO-ECC-END_2021-01-27_18-15-09", Epoch(2021, 1, 27.7605208333333), 0, 1],
+                   # ["IO-ECC-END_2021-01-27_118-15-09", Epoch(2021, 1, 31.9271875), 0, 1],
                    ["EU-OCC-END_2021-01-30_12-07-38", Epoch(2021, 1, 30.5053009259259), 1, 0],
                    ["EU-ECC-END_2021-01-16_06-54-18", Epoch(2021, 1, 16.2877083333333), 1, 1],
                    ["GA-OCC-END_2021-12-18_23-58-55", Epoch(2021, 12, 18.9992476851852), 2, 0],
                    ["GA-ECC-END_2021-12-19_04-36-37", Epoch(2021, 12, 19.1920949074074), 2, 1],
                    ["CA-OCC-END_2021-11-15_07-29-43", Epoch(2021, 11, 15.3123032407407), 3, 0],
-                   ["CA-ECC-END_2021-11-15_19-49-16", Epoch(2021, 11, 15.8258796296296), 3, 1],
+                   ["CA-ECC-END_2021-11-15_19-49-16", Epoch(2021, 11, 15.8258796296296), 3, 1]
                    ]
 
-    for epoch_label, epoch, row_idx, col_idx in test_values:
+    for test_label, epoch, row_idx, col_idx in test_set:
         result = JupiterMoons.is_phenomena(epoch)
         exp_event = False
         other_events: [[]] = []
@@ -153,7 +153,7 @@ def test_is_phenomena():
 
         # assert success, "Test failed!"
         # check.is_true(success, f"Test failed for {epoch_label}")
-        check.is_true(exp_event and len(other_events) == 0, f"Test={epoch_label}: "
+        check.is_true(exp_event and len(other_events) == 0, f"Test={test_label}: "
                                                             f"'expected event'={exp_event}, "
                                                             f"'other event(s)'={other_events}")
 
